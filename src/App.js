8946ec3d217bece8110faaaed23f18db4bf243f1
master-of-dobby <naveenoffcial777@gmail.com>
2024-04-06 11:55:57 +0530

    updated and created email based registration and integrated to view - used mysql db for storing user details

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 
 import Registration from "./Components/Registration";
 import Login from "./Components/Login";
+import Redirect from "./Components/Redirect";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route exact path="/" Component={Login}></Route>
           <Route path="/register" Component={Registration}></Route>
+          <Route path="/redirect/:email/:otp" Component={Redirect}></Route>
         </Routes>
       </Router>
     </>
--- src/Components/Registration.jsx
+++ src/Components/Registration.jsx
@@ -1,9 +1,36 @@
 import "../index.css";
-import React from "react";
+import React, { useState } from "react";
 import loginImage from "../common/login-reg.jpeg";
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 function Registration() {
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    location: "",
+  });
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      await axios.post("http://localhost:8080/user/register", formData);
+      //alert("User registered successfully!");
+      navigate("/", "/login");
+    } catch (err) {
+      console.error("Registration Failed ", err);
+    }
+  };
+
   return (
     <div className="login">
       <div className="login-theme">
@@ -18,41 +45,58 @@ function Registration() {
         </div>
 
         <div>
-          <form className="login-form-content">
+          <form className="login-form-content" onSubmit={handleSubmit}>
             {/* <label>Email</label> */}
             <input
               type="text"
-              name="firstname"
+              name="firstName"
               placeholder="First Name"
+              value={formData.firstName}
               required
+              onChange={handleChange}
             />{" "}
             <br />
             <input
               type="text"
-              name="firstname"
-              placeholder="First Name"
+              name="lastName"
+              placeholder="Last Name"
+              value={formData.lastName}
               required
+              onChange={handleChange}
             />{" "}
             <br />
-            <input type="email" name="email" placeholder="Email" required />
+            <input
+              type="email"
+              name="email"
+              placeholder="Email"
+              value={formData.email}
+              required
+              onChange={handleChange}
+            />
             <br />
             {/* <label>Password</label> */}
             <input
               type="text"
               name="password"
               placeholder="Password"
+              value={formData.password}
               required
+              onChange={handleChange}
             />
             <br />
             <input
               type="text"
-              name="firstname"
+              name="location"
               placeholder="Location"
+              value={formData.location}
               required
+              onChange={handleChange}
             />{" "}
             <br />
             <div className="button-container">
-              <button className="submit-btn" type="submit">Register</button> 
+              <button className="submit-btn" type="submit">
+                Register
+              </button>
               <Link to="/">
                 <button className="submit-btn">Login</button>
               </Link>
